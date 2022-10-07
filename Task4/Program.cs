efe3dc1596b245fffaa3e1b9e3c8c3f9eb54eb42
Nikita <shilo1488@bk.ru>
2022-10-07 19:16:11 +0300

    Решение задачи4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,1 +1,11 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+while (count <= N)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
